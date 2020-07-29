eb61f845a3f4ad09b5dae07c46ce761b0e00ff10
Guillermo A. Perez <guillermoalberto.perez@uantwerpen.be>
2020-07-29 09:06:17 +0200

    this is how we create the so in a pythonic way

---
+++ aiger/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for aiger
+"""
+
+from distutils.core import setup, Extension
+
+
+aiger_module = Extension("_aiger", sources=["aiger_wrap.c", "aiger.c"])
+
+setup(name="aiger",
+      version="1.0",
+      author="G. A. Perez",
+      description="""A swig wrapper for A. Biere's aiger""",
+      ext_modules=[aiger_module],
+      py_modules=["aiger"])
